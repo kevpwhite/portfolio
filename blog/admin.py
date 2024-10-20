6bf1d17782bd11761a8666ad3b378137c91a1aa0
Kevin White <kevinpwhitejr@gmail.com>
2024-10-20 02:54:43 -0700

    Blog issues

--- blog/admin.py
+++ blog/admin.py
@@ -3,16 +3,16 @@ from .models import Post, PostCategory, PostImage
 from mptt.admin import MPTTModelAdmin
 
 class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'slug', 'status','created_on', 'video_url')
     list_filter = ("status",)
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
 
 class PostCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'slug')
     list_filter = ("name",)
     search_fields = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
 
 class PostImageAdmin(admin.StackedInline):
     model = PostImage
