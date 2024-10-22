df5281ce8a62dea55fee7c437b0857dcc32f93da
Kevin White <kevinpwhitejr@gmail.com>
2024-10-22 11:49:22 -0700

    Fixed URL for CKEditor

--- kevinpwhite/urls.py
+++ kevinpwhite/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('sitemap.xml/', sitemap, {'sitemaps': sitemaps},
      name='django.contrib.sitemaps.views.sitemap'),
+    path('ckeditor5/', include('django_ckeditor_5.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
